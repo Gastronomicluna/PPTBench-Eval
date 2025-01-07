fd2fb09a9269472b2bd434d602d4512235a2951f
Tyrion Huu <tyrionhuu@gmail.com>
2025-01-07 19:21:24 +0800

    feat: add main function to execute dataset loading and display in main.py

---
+++ src/detection/main.py
@@ -0,0 +1,13 @@
+from src.shared.load_save_huggingface_dataset import load_save_huggingface_dataset_df
+
+def main():
+    dataset_name="tyrionhuu/PPTBench-Detection"
+    dataset_path="data/PPTBench-Detection"
+    df = load_save_huggingface_dataset_df(
+        dataset_name=dataset_name,
+        dataset_path=dataset_path,
+    )
+    print(df.head())
+    
+if __name__ == "__main__":
+    main()
