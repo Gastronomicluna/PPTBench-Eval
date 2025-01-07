fc59af5b0d6cc950baa7f04afdeffd711e5b7d90
Tyrion Huu <tyrionhuu@gmail.com>
2025-01-07 18:41:14 +0800

    refactor: rename SUBCATEGORY_JUDGE_FUNCTION_MAP to SUBCATEGORY_JUDGE_FUNCTION in utils.py

--- src/detection/utils.py
+++ src/detection/utils.py
@@ -1,6 +1,6 @@
 from .judge import compare_coordinate, exact_match, fuzzy_match
 
-SUBCATEGORY_JUDGE_FUNCTION_MAP = {
+SUBCATEGORY_JUDGE_FUNCTION = {
     "content extraction": fuzzy_match,
     "layout detection": compare_coordinate,
     "style detection": exact_match,
