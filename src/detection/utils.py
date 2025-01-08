12487636e727994bd156f706cf57b9ebcf857f93
Tyrion Huu <tyrionhuu@gmail.com>
2025-01-08 13:00:38 +0800

    refactor: add API LLM models list to utils.py

--- src/detection/utils.py
+++ src/detection/utils.py
@@ -5,3 +5,13 @@ SUBCATEGORY_JUDGE_FUNCTION = {
     "layout detection": compare_coordinate,
     "style detection": exact_match,
 }
+
+API_LLM_MODELS = [
+    "claude-3-5-sonnet-20241022",
+    "gpt-4o-2024-11-20",
+    "o1-2024-12-17",
+    "gemini-2.0-flash-exp",
+    "gemini-2.0-flash-thinking-exp",
+    "qwen-vl-max-0809",
+    "llama-3.2-90b-vision-instruct"
+]
