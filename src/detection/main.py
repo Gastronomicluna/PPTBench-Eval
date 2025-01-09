4bd9f61e9ab4e8262dee0f2d454fbcde676e067d
Tianyu Hu <tyrionhuu@gmail.com>
2025-01-09 21:49:31 +0800

    fix: enable force download for dataset loading in main.py

--- src/detection/main.py
+++ src/detection/main.py
@@ -135,7 +135,7 @@ def main(
         df = load_save_dataset_df(
             dataset_name=dataset_name,
             dataset_path=dataset_path,
-            force_download=False,
+            force_download=True,
             source="huggingface",
         )
 
