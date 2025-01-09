0a3f19c921cd820430f84fa694dc9f12fb17bffa
Tianyu Hu <tyrionhuu@gmail.com>
2025-01-09 21:12:48 +0800

    feat: implement formatting functions for content extraction and style detection answers

--- src/detection/format_answers.py
+++ src/detection/format_answers.py
@@ -1,1 +1,34 @@
-from ..shared.parse_answer import parse_answer
+from ..shared.parse_answer import parse_json_answer
+
+def format_content_extraction_answer(
+    answer: str,
+) -> str:
+    """
+    Format the extracted content for content extraction tasks.
+
+    Args:
+        answer (str): The extracted content.
+
+    Returns:
+        str: The formatted answer.
+    """
+    json_answer = parse_json_answer(answer)
+    answer = json_answer["answer"]
+    return answer
+
+def format_style_detection_answer(
+    answer: str,
+) -> str:
+    """
+    Format the extracted style for style detection tasks.
+
+    Args:
+        answer (str): The extracted style.
+
+    Returns:
+        str: The formatted answer.
+    """
+    json_answer = parse_json_answer(answer)
+    answer = json_answer["answer"]
+    return answer
+
