82b57c08c2c12b1aea0de5fc1e10b6ba9fedb60a
Tianyu Hu <tyrionhuu@gmail.com>
2025-01-09 22:03:30 +0800

    refactor: change force_download parameter to False in load_save_dataset_df function

--- src/detection/main.py
+++ src/detection/main.py
@@ -141,7 +141,7 @@ def main(
         df = load_save_dataset_df(
             dataset_name=dataset_name,
             dataset_path=dataset_path,
-            force_download=True,
+            force_download=False,
             source="huggingface",
         )
 
