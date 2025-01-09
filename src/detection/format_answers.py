e844aa8283e2c730ca49e6e69c2e10ab08007569
Tianyu Hu <tyrionhuu@gmail.com>
2025-01-09 21:16:13 +0800

    feat: enhance answer formatting functions to support layout detection and update return types

--- src/detection/format_answers.py
+++ src/detection/format_answers.py
@@ -1,5 +1,5 @@
 from ..shared.parse_answer import parse_json_answer
-
+from typing import Dict, Any, Union
 def format_content_extraction_answer(
     answer: str,
 ) -> str:
@@ -18,7 +18,7 @@ def format_content_extraction_answer(
 
 def format_style_detection_answer(
     answer: str,
-) -> str:
+) -> Union[str, int]:
     """
     Format the extracted style for style detection tasks.
 
@@ -26,9 +26,23 @@ def format_style_detection_answer(
         answer (str): The extracted style.
 
     Returns:
-        str: The formatted answer.
+        Union[str, int]: The formatted answer.
     """
     json_answer = parse_json_answer(answer)
     answer = json_answer["answer"]
     return answer
 
+def format_layout_detection_answer(
+    answer: str,
+) -> Dict[str, Any]:
+    """
+    Format the detected layout for layout detection tasks.
+
+    Args:
+        answer (str): The detected layout.
+
+    Returns:
+        Dict[str, int]: The formatted answer.
+    """
+    json_answer = parse_json_answer(answer)
+    return json_answer
