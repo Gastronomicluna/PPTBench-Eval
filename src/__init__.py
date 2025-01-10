1cf13c2a73b5f07b4f3efa3593c64a7a16154ec2
Tyrion Huu <tyrionhuu@gmail.com>
2025-01-10 19:00:15 +0800

    chore: add __init__.py files to src and modification directories and configure pytest

---
+++ src/__init__.py
@@ -0,0 +1,9 @@
+# Create an empty __init__.py file in the src directory and its subdirectories
+# This will make the src directory a proper Python package
+
+# Add pytest.ini to configure the Python path
+# [pytest]
+# pythonpath = src
+
+# Update the import statement in the test file
+# from src.module import function
