c832ee776fd6b3cabf4fbeee3063dcba5b04629e
Tyrion Huu <tyrionhuu@gmail.com>
2025-01-18 20:11:47 +0800

    refactor: improve font comparison logic in judge_answer_change_font function

--- src/modification/judge/judge_change_font.py
+++ src/modification/judge/judge_change_font.py
@@ -2,7 +2,7 @@ import logging
 from typing import Any, Dict, List
 
 from ...shared.pptx_api.api_executor import api_executor
-from ..utils import get_font, get_font_from_shape, get_shape_from_presentation
+from ..utils import get_slide_from_presentation, get_font, get_font_from_shape, get_shape_from_presentation, get_shape_from_slide
 
 
 def judge_answer_change_font(
@@ -27,49 +27,47 @@ def judge_answer_change_font(
     slide_id = ground_truth.get("slide", {}).get("slide_id")
 
     # Execute the API calls
-    modified_presentation = api_executor(
+    llm_modified_presentation = api_executor(
         lines=api_calls, 
         json=presentation_json, 
         mode="json"
     )
-    if modified_presentation is None:
+    if llm_modified_presentation is None:
         logging.error("Error executing API calls, result is None.")
         return False
+    
+    # Get the shape from the slide
+    llm_modified_shape = get_shape_from_presentation(
+        slide_id=slide_id,
+        shape_id=shape_id,
+        presentation=llm_modified_presentation,
+    )
+    llm_modified_font = get_font_from_shape(llm_modified_shape)
+    if llm_modified_font is None:
+        logging.error("Error getting font names from the result JSON.")
+        return False
+    if len(llm_modified_font) > 1:
+        logging.error("More than one font name found in the result JSON.")
+        return False
+
+    llm_modified_font_name = llm_modified_font.pop()
     
     # Get the shape ID from the ground truth
     shape_id = shape_to_modify["shape_id"]
 
     # Get the font names from the ground truth
-    font = get_font(
+    gold_font = get_font(
         shape_id=shape_id,
         ground_truth=ground_truth,
     )
 
-    if font is None:
+    if gold_font is None:
         logging.error("Error getting font names from the ground truth.")
         return False
-    if len(font) > 1:
+    if len(gold_font) > 1:
         logging.error("More than one font name found in the ground truth.")
         return False
 
-    font_name = font.pop()
+    gold_font_name = gold_font.pop()
 
-
-
-    # Get the shape from the slide
-    result_shape = get_shape_from_presentation(
-        slide_id=slide_id,
-        shape_id=shape_id,
-        presentation=modified_presentation,
-    )
-    result_font = get_font_from_shape(result_shape)
-    if result_font is None:
-        logging.error("Error getting font names from the result JSON.")
-        return False
-    if len(result_font) > 1:
-        logging.error("More than one font name found in the result JSON.")
-        return False
-
-    result_font_name = result_font.pop()
-
-    return font_name == result_font_name
+    return gold_font_name == llm_modified_font_name
