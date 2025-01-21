2029b5b0f61bf7e3492b903f5f597605a6657c33
Tyrion Huu <tyrionhuu@gmail.com>
2025-01-21 14:50:36 +0800

    feat: refactor generate_pptx.py to integrate PNG generation and improve path handling

--- src/generation/generate_pptx.py
+++ src/generation/generate_pptx.py
@@ -4,35 +4,8 @@ from ..shared.pptx_api.api_executor import api_executor
 from ..shared.utils import pptx_to_png, generate_hash
 
 
-def generate_png_files_from_pptx_files(
-    df: pd.DataFrame,
-    base_dir: str,
-) -> pd.DataFrame:
-    """
-    Generate the PNG files from the PowerPoint files in the DataFrame.
 
-    Args:
-        df (pd.DataFrame): The DataFrame containing the PowerPoint files.
-        base_dir (str): The base directory for the PNG files.
-
-    Returns:
-        pd.DataFrame: The DataFrame with the generated PNG files.
-    """
-    for index, row in df.iterrows():
-        pptx_path = row["pptx_path"]
-        hash_str = row["hash"]
-        png_path = build_png_path(
-            base_dir=base_dir,
-            hash_str=hash_str,
-        )
-        pptx_to_png(
-            pptx_path=pptx_path,
-            png_path=png_path,
-        )
-        df.at[index, "png_path"] = png_path
-    return df
-
-def generate_pptx_files(
+def generate_pptx_files_with_png_files(
     df: pd.DataFrame,
     base_dir: str,
 ) -> pd.DataFrame:
@@ -55,11 +28,20 @@ def generate_pptx_files(
             base_dir=base_dir,
             hash_str=hash_str,
         )
+        png_path = build_png_path(
+            base_dir=base_dir,
+            hash_str=hash_str,
+        )
         generate_pptx(
             api_calls=api_calls,
             pptx_path=pptx_path,
         )
+        pptx_to_png(
+            pptx_path=pptx_path,
+            png_path=png_path,
+        )
         df.at[index, "pptx_path"] = pptx_path
+        df.at[index, "png_path"] = png_path
     return df
 
 
@@ -84,22 +66,22 @@ def generate_pptx(
 
 def build_png_path(
     base_dir: str,
-    hash_str: str,
+    file_name: str,
 ) -> str:
     """
     Build the path to the PNG file based on the hash.
     
     Args:
         base_dir (str): The base directory for the PNG file.
-        hash_str (str): The hash string.
+        file_name (str): The hash string.
         
     Returns:
         str: The path to the PNG file.
     """
-    return f"{base_dir}/png/{hash_str}.png"
+    return f"{base_dir}/png/{file_name}.png"
 def build_pptx_path(
     base_dir: str,
-    hash_str: str,
+    file_name: str,
 ) -> str:
     """
     Build the path to the PowerPoint file based on the task and hash.
@@ -107,9 +89,9 @@ def build_pptx_path(
     Args:
         base_dir (str): The base directory for the PowerPoint file.
         task (str): The task name.
-        hash_str (str): The hash string.
+        file_name (str): The hash string.
 
     Returns:
         str: The path to the PowerPoint file.
     """
-    return f"{base_dir}/pptx/{hash_str}.pptx"
+    return f"{base_dir}/pptx/{file_name}.pptx"
