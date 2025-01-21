d13dd839d33cf079ee85572e7e4f2a1446c846c8
Tyrion Huu <tyrionhuu@gmail.com>
2025-01-21 16:23:21 +0800

    refactor: remove unused import of download_kaggle_dataset in main.py for improved code clarity

--- src/generation/main.py
+++ src/generation/main.py
@@ -9,7 +9,7 @@ from ..shared.format_answers_api import format_answer_csv
 from ..shared.get_answer import get_answers
 from ..shared.llm import API_LLM_MODELS
 from ..shared.load_save_dataset import load_save_dataset_df
-from ..shared.utils import download_kaggle_dataset, get_project_root, process_model
+from ..shared.utils import get_project_root, process_model  # download_kaggle_dataset,
 from .generate_pptx import generate_pptx_files_csv
 from .get_answers import get_answer_single_generation
 
