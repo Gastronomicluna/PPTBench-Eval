259ab4b4ff09bcb70649c06afddbf47e88126f80
Tyrion Huu <tyrionhuu@gmail.com>
2025-01-21 14:23:03 +0800

    feat: reorder import statements in main.py for consistency

--- src/modification/main.py
+++ src/modification/main.py
@@ -5,12 +5,12 @@ from typing import Dict
 
 import pandas as pd
 
+from ..shared.format_answers_api import format_answer_csv
 from ..shared.get_answer import get_answers
 from ..shared.llm import API_LLM_MODELS
 from ..shared.load_save_dataset import load_save_dataset_df
 from ..shared.utils import download_kaggle_dataset, get_project_root, process_model
 from .evaluation import evaluate_answers
-from ..shared.format_answers_api import format_answer_csv
 from .get_answers import get_answer_single_modification
 from .judge import judge_answer_df
 
