5971734fc5b3b8395dd509f4d8bc619cd82c3180
Tyrion Huu <tyrionhuu@gmail.com>
2025-01-21 10:15:45 +0800

    feat: add content_images parameter to build_prompt function for enhanced prompt customization

--- src/generation/prompts.py
+++ src/generation/prompts.py
@@ -17,6 +17,7 @@ def build_prompt(
     query: str,
     task: Literal["note_to_slide", "multimedia_to_slide", "screenshot_to_slide", "text_to_slide"],
     slide_json: Dict[str, Any],
+    content_images: List[str] = [],
 ) -> str:
     """
     Build the prompt for the given query.
@@ -24,32 +25,16 @@ def build_prompt(
     Args:
         query (str): The query to build the prompt for.
         slide_json (Dict[str, Any]): The JSON data for the slide.
-
+        task (Literal["note_to_slide", "multimedia_to_slide", "screenshot_to_slide", "text_to_slide"]): 
+            The task to build the prompt for.
+        content_images (List[str], optional): The list of content images. Defaults to
+        
     Returns:
         str: The prompt for the query.
     """
-    pass
-    divider = "#" * 80
-    example_json_str = json.dumps(GENERATION_EXAMPLE, indent=2)
+    if task == "note_to_slide":
+        return build_prompt_for_note_to_slide(query=query, slide_json=slide_json, content_images=content_images)
 
-    prompt = ""
-    prompt += "Task: You are given a slide from a presentation in the form of an image and JSON data.\n"
-    prompt += f"{query}\n"
-    prompt += "To achieve this task, you can use the following functions:\n"
-    prompt += f"{api_to_string(api_list)}\n\n"
-    prompt += "Instructions:\n"
-    prompt += "- Return in JSON format only the requested information without any additional text or explanations.\n"
-    prompt += "- Abide by JSON formatting rules.\n\n"
-    prompt += "Examples:\n"
-    prompt += f"{example_json_str}\n\n"
-    prompt += f"{divider}\n"
-    prompt += "Slide JSON:\n"
-    prompt += f"{json.dumps(slide_json, indent=2)}\n\n"
-    prompt += f"{divider}\n"
-    prompt += f"Query: {query}\n"
-    prompt += "Answer:\n"
-
-    return prompt
 
 
 def build_prompt_for_note_to_slide(
@@ -63,7 +48,8 @@ def build_prompt_for_note_to_slide(
     Args:
         query (str): The query to build the prompt for.
         slide_json (Dict[str, Any]): The JSON data for the slide.
-
+        content_images (List[str], optional): The list of content images. Defaults to [].
+        
     Returns:
         str: The prompt for the query.
     """
