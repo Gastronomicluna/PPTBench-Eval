e61dcdbf7c54c0639e8237f2fdac9bfc05a8e805
Tyrion Huu <tyrionhuu@gmail.com>
2025-01-22 10:12:53 +0800

    fix: update import statement in main.py to use get_answers_detection for consistency

--- src/detection/main.py
+++ src/detection/main.py
@@ -5,12 +5,12 @@ from typing import Dict
 
 import pandas as pd
 
-from .get_answers import get_answers_detection
 from ..shared.llm import API_LLM_MODELS
 from ..shared.load_save_dataset import load_save_dataset_df
 from ..shared.utils import get_project_root, process_model
 from .evaluation import evaluate_answers
 from .format_answers import format_answer_csv
+from .get_answers import get_answers_detection
 from .judge import judge_answer_df
 
 
