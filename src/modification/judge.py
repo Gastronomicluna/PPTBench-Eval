0c9a45d0ec16ceff29e8406511b0a9e5a6ec1643
Tyrion Huu <tyrionhuu@gmail.com>
2025-01-24 16:09:26 +0800

    refactor: replace ground_truth parameter with json_data in judge_answer function

--- src/modification/judge.py
+++ src/modification/judge.py
@@ -131,21 +131,20 @@ def judge_answer(
         return judge_answer_reposition(
             api_calls=api_calls,
             shape_to_modify=shape_to_modify,
-            ground_truth=ground_truth,
+            json_data=json_data,
             presentation_json=presentation_json,
         )
     elif task == "resize":
         return judge_answer_resize(
             api_calls=api_calls,
             shape_to_modify=shape_to_modify,
-            ground_truth=ground_truth,
+            json_data=json_data,
             presentation_json=presentation_json,
         )
     elif task == "overlap" or task == "out_of_bounds":
         return judge_answer_refinement(
             api_calls=api_calls,
             json_data=json_data,
-            ground_truth=ground_truth,
             presentation_json=presentation_json,
         )
     else:
