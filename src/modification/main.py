f01a8970fe7c2093af1ca284763ec95c693df963
Tyrion Huu <tyrionhuu@gmail.com>
2025-02-02 13:16:10 +0800

    fix: update filter condition in main function for test mode

--- src/modification/main.py
+++ src/modification/main.py
@@ -85,7 +85,7 @@ def main(
 
     # Test mode
     if test_mode:
-        df = df[df["subcategory"] == "element_modification"]
+        df = df[df["task"] == "resize_shape"]
         # df = df[df["task"] == "reposition"]
         df = df.sample(20, random_state=42)
 
