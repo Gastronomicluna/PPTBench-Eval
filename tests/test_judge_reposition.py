6063fed3074b3ce662d923f3520d84cd19c876d1
Tianyu Hu <tyrionhuu@gmail.com>
2025-02-10 11:40:02 +0800

    refactor: update shape_to_modify fixture and add new api_calls and json_data fixtures in test_judge_reposition.py

--- tests/test_judge_reposition.py
+++ tests/test_judge_reposition.py
@@ -14,28 +14,15 @@ def base_presentation_json():
 
 @pytest.fixture
 def shape_to_modify():
-    return {
-        "name": "PlaceHolder 1",
-        "shape_id": 17,
-        "shape_type": "PLACEHOLDER",
-        "measurement_unit": "emu",
-        "height": 533520,
-        "width": 7772400,
-        "left": 685800,
-        "top": 152280,
-        "text": "Near Term Priorities (Booster)",
-        "font_details": [
-            {
-                "paragraph_index": 0,
-                "run_index": 0,
-                "text": "Near Term Priorities (Booster)",
-                "font_name": "Comic Sans MS",
-                "font_size": 24.0,
-            }
-        ],
-        "placeholder_type": "TITLE",
-    }
+    return {"name": "PlaceHolder 1", "shape_id": 149, "shape_type": "PLACEHOLDER", "measurement_unit": "emu", "height": 685800, "width": 9144000, "left": 0, "top": 609120, "text": "Consumer Information and Disclosure ", "font_details": [{"paragraph_index": 0, "run_index": 0, "text": "Consumer Information and Disclosure", "font_name": "Verdana", "font_size": 32.0}, {"paragraph_index": 0, "run_index": 1, "text": " ", "font_name": "Verdana", "font_size": 32.0}], "placeholder_type": "TITLE"}
 
+@pytest.fixture
+def api_calls():
+    return ['choose_slide(266)', 'choose_shape(149)', 'set_top(0)']
+
+@pytest.fixture
+def json_data():
+    return {"slide_width": 9144000, "slide_height": 6858000, "measurement_unit": "emu", "slide": {"slide_id": 266, "slide_name": "", "shapes": [{"name": "PlaceHolder 1", "shape_id": 149, "shape_type": "PLACEHOLDER", "measurement_unit": "emu", "height": 685800, "width": 9144000, "left": 0, "top": 0, "text": "Consumer Information and Disclosure ", "font_details": [{"paragraph_index": 0, "run_index": 0, "text": "Consumer Information and Disclosure", "font_name": "Verdana", "font_size": 32.0}, {"paragraph_index": 0, "run_index": 1, "text": " ", "font_name": "Verdana", "font_size": 32.0}], "placeholder_type": "TITLE"}, {"name": "PlaceHolder 2", "shape_id": 150, "shape_type": "PLACEHOLDER", "measurement_unit": "emu", "height": 4343400, "width": 8534160, "left": 304920, "top": 1599840, "text": "Greater transparency in college costs\nED required to develop & release:\nInformation relevant to college cost & net price by institution \nCollege cost & net price watch lists\nInternet-based calculators of the net price of college for consumers", "font_details": [{"paragraph_index": 0, "run_index": 0, "text": "Greater transparency in college costs", "font_name": "Verdana", "font_size": 30.0}, {"paragraph_index": 1, "run_index": 0, "text": "ED required to develop & release:", "font_name": "Verdana", "font_size": 30.0}, {"paragraph_index": 2, "run_index": 0, "text": "Information relevant to college cost & net price by institution ", "font_name": "Verdana", "font_size": 30.0}, {"paragraph_index": 3, "run_index": 0, "text": "College cost & net price watch lists", "font_name": "Verdana", "font_size": 30.0}, {"paragraph_index": 4, "run_index": 0, "text": "Internet-based calculators of the net price of college for consumers", "font_name": "Verdana", "font_size": 30.0}], "placeholder_type": "OBJECT"}, {"name": "Slide Number Placeholder 3", "shape_id": 151, "shape_type": "AUTO_SHAPE", "measurement_unit": "emu", "height": 457200, "width": 1905120, "left": 7086600, "top": 6400800, "text": "<number>", "font_details": []}]}}
 
 def test_shape_reposition_score_exact_match(shape_to_modify):
     """Test score calculation with exactly matching shapes."""
