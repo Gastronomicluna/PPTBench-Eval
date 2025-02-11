efd85ee86a3c3c5e3c9d0141775ad6ad04932800
Tianyu Hu <tyrionhuu@gmail.com>
2025-02-11 13:39:32 +0800

    add debug

--- src/shared/pptx_api/api_executor_json.py
+++ src/shared/pptx_api/api_executor_json.py
@@ -112,7 +112,7 @@ def choose_slide(slide_id: int) -> None:
     slides = JSON_DATA.get("slides", [])
     print("Target slide ID:", slide_id)
     for slide in slides:
-        # print(slide)
+        print(slide["slide_id"])
         if slide["slide_id"] == slide_id:
             JSON_CURRENT_SLIDE = slide
             print("Slide chosen:", slide_id)
