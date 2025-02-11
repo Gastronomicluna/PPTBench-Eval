60e7f9a8ac081f2dbbc56c4161855d32d18a5ad2
Tianyu Hu <tyrionhuu@gmail.com>
2025-02-11 12:40:45 +0800

    fix: comment out debug print statement in judge_answer function for cleaner output

--- src/modification/judge.py
+++ src/modification/judge.py
@@ -154,7 +154,7 @@ def judge_answer(
             presentation_json=presentation_json,
         )
     elif task == "reposition_shape":
-        print("api_calls: ", api_calls)
+        # print("api_calls: ", api_calls)
         # print("shape_to_modify: ", shape_to_modify)
         # print("json_data: ", json_data)
         # print("presentation_json: ", presentation_json)
