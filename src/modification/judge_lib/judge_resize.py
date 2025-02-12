3d4eb2c77a3bdfaf6666ee894e3557789cb43924
Tianyu Hu <tyrionhuu@gmail.com>
2025-02-12 12:13:08 +0800

    add debug

--- src/modification/judge_lib/judge_resize.py
+++ src/modification/judge_lib/judge_resize.py
@@ -57,7 +57,7 @@ def judge_answer_resize(
         shape_id=shape_id,
         presentation=modified_presentation_json,
     )
-
+    print("Modified shape:", modified_shape)
     ground_truth_shape = shape_to_modify
 
     # Compare the shapes
