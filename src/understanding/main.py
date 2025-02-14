13c5ece5092dad2b4974011fa176c8daff136b1c
Tianyu Hu <tyrionhuu@gmail.com>
2025-02-14 12:03:22 +0800

    modify

--- src/understanding/main.py
+++ src/understanding/main.py
@@ -55,7 +55,7 @@ def main(
     df = load_save_dataset_df(
         dataset_name=dataset_name,
         dataset_path=dataset_path,
-        force_download=False,
+        force_download=True,
         source="huggingface",
     )
 
