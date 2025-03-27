cddeb4730863dfc4fcd785bd286dc92401df6830
Tyrion Huu <tyrionhuu@gmail.com>
2025-03-27 20:02:33 +0800

    refactor: remove commented-out import for cleaner code in create_layout.py

--- src/generation/agentic_generation/create_layout.py
+++ src/generation/agentic_generation/create_layout.py
@@ -5,9 +5,11 @@ from typing import List, Literal
 
 from src.shared.llm import call_vision_model
 from src.shared.parse_answer import parse_api_calls
-# from src.shared.utils import str_to_list
 
 from .prompts import build_create_layout_prompt
+
+# from src.shared.utils import str_to_list
+
 
 
 def create_presentation(
